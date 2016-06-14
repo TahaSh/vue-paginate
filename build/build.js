50c965280bb6d3376f4e56580521e897b3086a62
Taha Shashtari <taha.shashtari@gmail.com>
2016-06-15 00:03:11 +0300

    Fix commonjs resolve module issue

---
+++ build/build.js
@@ -0,0 +1,68 @@
+var fs = require('fs')
+var zlib = require('zlib')
+var rollup = require('rollup')
+var uglify = require('uglify-js')
+var babel = require('rollup-plugin-babel')
+var version = process.env.VERSION || require('../package.json').version
+var banner =
+  '/*!\n' +
+  ' * vue-paginate v' + version + '\n' +
+  ' * (c) ' + new Date().getFullYear() + ' Taha Shashtari\n' +
+  ' * Released under the MIT License.\n' +
+  ' */'
+
+rollup.rollup({
+  entry: 'src/index.js',
+  plugins: [
+    babel({
+      presets: ['es2015-rollup']
+    })
+  ]
+})
+.then(function (bundle) {
+  return write('dist/vue-paginate.js', bundle.generate({
+    format: 'umd',
+    banner: banner,
+    moduleName: 'VuePaginate'
+  }).code)
+})
+.then(function () {
+  return write(
+    'dist/vue-paginate.min.js',
+    banner + '\n' + uglify.minify('dist/vue-paginate.js').code
+  )
+})
+.then(function () {
+  return new Promise(function (resolve, reject) {
+    fs.readFile('dist/vue-paginate.min.js', function (err, buf) {
+      if (err) return reject(err)
+      zlib.gzip(buf, function (err, buf) {
+        if (err) return reject(err)
+        write('dist/vue-paginate.min.js.gz', buf).then(resolve)
+      })
+    })
+  })
+})
+.catch(logError)
+
+function write (dest, code) {
+  return new Promise(function (resolve, reject) {
+    fs.writeFile(dest, code, function (err) {
+      if (err) return reject(err)
+      console.log(blue(dest) + ' ' + getSize(code))
+      resolve()
+    })
+  })
+}
+
+function getSize (code) {
+  return (code.length / 1024).toFixed(2) + 'kb'
+}
+
+function logError (e) {
+  console.log(e)
+}
+
+function blue (str) {
+  return '\x1b[1m\x1b[34m' + str + '\x1b[39m\x1b[22m'
+}
